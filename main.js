6c7b6dbc746ec85731701a8b8e117d31894f4347
RyanTwT354 <84882715+RyanTwT354@users.noreply.github.com>
2021-08-13 17:25:05 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+status = "";
+
 function preload() {
   video = createVideo('video.mp4');
 }
@@ -8,7 +10,19 @@ function setup() {
   video.hide();
 }
 
-function start();
+function Start() {
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+  
+}
+
+function modelLoaded() {
+  console.log("modelLoaded");
+  status = true;
+  video.loop();
+  video.speed(1);
+  video.volume(0);
+}
+
 
 function draw() {
   image(video, 0, 0, 500, 400);
